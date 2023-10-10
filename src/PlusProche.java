89f331bb43172ed593ab5d5d15fbbfc76f8b2ac4
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-10-10 11:34:21 +0200

    Q2.2

--- src/PlusProche.java
+++ src/PlusProche.java
@@ -32,9 +32,13 @@ public class PlusProche extends AlgoClassification
         return plusProche.etiquette;
     }
 
-    public double calculerDistance(Imagette test, Imagette entrainement)
+    public double calculerDistance(Imagette imagette1, Imagette imagette2)
     {
-
+        double somme = 0;
+        for (int i = 0 ; i < imagette1.pixels.length ; i ++)
+            for (int j = 0 ; j < imagette2.pixels.length ; j ++)
+                somme += Math.pow(imagette1.pixels[i][j] - imagette2.pixels[i][j], 2);
+        return Math.sqrt(somme);
     }
 
 
