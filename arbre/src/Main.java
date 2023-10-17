75446fca01f302f47c7dc33941b6b757118f37b2
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-10-17 16:50:11 +0200

    TP2 Q1

---
+++ arbre/src/Main.java
@@ -0,0 +1,19 @@
+import chargement.Chargement;
+import chargement.Data;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        Chargement ch = new Chargement("arbre/data/data_animaux/animaux.csv");
+        List<Data> dataAnimaux = ch.chargerFichier();
+
+        for (Data animal : dataAnimaux)
+        {
+            animal.ajouterAttribut("alea", String.valueOf(Math.random()));
+            System.out.println(animal.getValeur("alea"));
+        }
+    }
+}
